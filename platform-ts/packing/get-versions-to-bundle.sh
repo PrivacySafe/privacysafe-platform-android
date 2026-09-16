57497b223620426c98ae382d1e89a58f097f802d
privacysafe-team <support@ivycyber.com>
2026-09-16 01:28:48 +0000

    v0.21.11

--- platform-ts/packing/get-versions-to-bundle.sh
+++ platform-ts/packing/get-versions-to-bundle.sh
@@ -8,53 +8,41 @@ source packing/versioning.sh
 
 brand="PrivacySafe"
 channel="nightly"
-versions_dir="dist/bundle.versions"
+versions_file="dist/bundle.versions"
 
 echo "---------- Task overview ----------------
-This collects apps' versions info into $versions_dir to make $brand $channel bundle.
+This collects apps' versions into to make $brand $channel bundle.
 -----------------------------------------
 "
 
-platform_desktop_ver="$(
-	get_platform_latest_version_in_channel $brand $channel || exit $?
-)"
-if [ -z "$platform_desktop_ver" ]
-then
-	echo "Got no latest version from $channel channel in $(platform_url $brand)/"
-	exit 1
-fi
-
-conf_json=$(get_config_of $brand $platform_desktop_ver)
-
-if [ -z "$conf_json" ]
-then
-	echo "Fail to get $brand configuration from desktop"
-	exit -2
-fi
-
-rm -r $versions_dir 2> /dev/null
-mkdir -p $versions_dir || exit $?
-apps_dir="$versions_dir/apps"
-mkdir $apps_dir || exit $?
-app_packs_dir="$versions_dir/app-packs"
-mkdir $app_packs_dir || exit $?
-
-launcher_domain=$(get_from_json_file $conf_json launcher-app)
-get_app_latest_version_in_channel $brand $launcher_domain $channel > $apps_dir/$launcher_domain || exit $?
-echo $launcher_domain "->" $(cat $apps_dir/$launcher_domain)
-
-startup_domain=$(get_from_json_file $conf_json startup-app)
-get_app_latest_version_in_channel $brand $startup_domain $channel > $apps_dir/$startup_domain || exit $?
-echo $startup_domain "->" $(cat $apps_dir/$startup_domain)
-
-for app_domain in $(get_from_json_file $conf_json bundled-apps)
+packs_dir="dist/bundled-app-packs"
+apps_dir="dist/bundled-apps"
+for dir in $packs_dir $apps_dir
 do
-	get_app_latest_version_in_channel $brand $app_domain $channel > $apps_dir/$app_domain || exit $?
-	echo $app_domain "->" $(cat $apps_dir/$app_domain)
+	if [ -d $dir ]
+	then
+		rm -r $dir
+	fi
+	mkdir -p $dir
 done
 
-for app_domain in $(get_from_json_file $conf_json bundled-app-packs)
+channel="nightly"
+versions_url="https://3nsoft.com/downloads/$brand/$channel/android/versions-in-bundle.json"
+
+curl --silent "$versions_url" --output $versions_file
+
+apps="$(cat $versions_file | jq -r .apps | jq -r keys[])"
+for app in $apps
 do
-	get_app_latest_version_in_channel $brand $app_domain $channel > $app_packs_dir/$app_domain || exit $?
-	echo $app_domain "->" $(cat $app_packs_dir/$app_domain)
+	app_version="$(cat $versions_file | jq -r .apps[\"$app\"])"
+	bash packing/download-app.sh $(app_zip_url $brand $app $app_version) $app $apps_dir "unzip" || exit $?
+	echo
+done
+
+app_packs="$(cat $versions_file | jq -r .\"app-packs\" | jq -r keys[])"
+for app in $app_packs
+do
+	app_version="$(cat $versions_file | jq -r .\"app-packs\"[\"$app\"])"
+	bash packing/download-app.sh $(app_zip_url $brand $app $app_version) $app $packs_dir "unzip" || exit $?
+	echo
 done
